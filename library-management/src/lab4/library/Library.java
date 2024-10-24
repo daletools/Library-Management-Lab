896a2a5a25553ef37225f353f45d721783b6b4de
jjsyatco <jjsyatco@yahoo.com>
2024-10-24 15:28:02 -0700

    Constructors and Setters implemented
    Added constructor and  setter to Book, LibrarianController, Library and Member class

--- library-management/src/lab4/library/Library.java
+++ library-management/src/lab4/library/Library.java
@@ -1,16 +1,31 @@
 package lab4.library;
 
+import java.util.ArrayList;
+
 public class Library {
-	
-	//  GRASP Principles: Creator, Controller
-	//	Manage the catalog of books and members.
-	//	Track which books are borrowed and available
-	
-	
+
+	// GRASP Principles: Creator, Controller
+	// Manage the catalog of books and members.
+	// Track which books are borrowed and available
+
 	// Attributes:
-	// private catalog (list of Book) 
+	// private catalog (list of Book)
 	// private members (list of Members)
+	private ArrayList<Book> catalog;
+	private ArrayList<Member> members;
 
 	// TODO: implement functionality of Member class
+	public Library() {
+		this.catalog = new ArrayList<>();
+		this.members = new ArrayList<>();
+	}
+
+	public ArrayList<Book> getCatalog() {
+		return catalog;
+	}
+
+	public ArrayList<Member> getMembers() {
+		return members;
+	}
 
 }
--- library-management/src/lab4/library/Member.java
+++ library-management/src/lab4/library/Member.java
@@ -1,13 +1,28 @@
 package lab4.library;
 
+import java.util.ArrayList;
+
 public class Member {
 
 	// GRASP Principle: Information Expert and Low Coupling
 	// Can borrow and return books.
 
 	private String name;
+	private ArrayList<Book> borrowedBooks;
 	// private borrowedbBooks TODO: implement collection of borrowed books
-	
+
 	// TODO: implement functionality of Member class
+	public Member(String name) {
+		this.name = name;
+		this.borrowedBooks = new ArrayList<>();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public ArrayList<Book> getBorrowedBooks() {
+		return borrowedBooks;
+	}
 
 }
